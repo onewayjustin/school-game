4f35e2232189a6c7b040b471f930111716140b45
onewayjustin <150301983+onewayjustin@users.noreply.github.com>
2024-02-26 17:30:33 -0500

    Add files via upload

---
+++ camera.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    public float speed;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 desiredPos = player.position + offset;
+        transform.position = Vector3.Lerp(transform.position, desiredPos, speed * Time.deltaTime);
+    }
+}
